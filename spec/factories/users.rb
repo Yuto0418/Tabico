b6f976d14c736d3abf129139bfb2f5afef57d24a
Yuto Kawai <yuto@YutonoMacBook-Air.local>
2021-10-12 12:31:36 +0900

    rspec requestテスト ログイン項目 追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    name { 'テスト用ユーザー' }
+    email { 'test@gmail.com' }
+    password = 'test1234'
+    password { password }
+    password_confirmation { password }
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  let(:user) { FactoryBot.create(:user) }
+
   describe "GET /index" do
-    it "returns http success" do
-      get "/users/index"
-      expect(response).to have_http_status(:success)
+    it "トップページが正常なレスポンスを返すこと" do
+      get "/users"
+      expect(response).to be_successful
+      expect(response).to have_http_status "200"
     end
   end
 
+  describe "GET #new" do
+    it "新規登録ページが正常なレスポンスを返すこと" do
+      get new_user_registration_path
+      expect(response).to be_successful
+      expect(response).to have_http_status "200"
+    end
+
+    it "ログインページが正常なレスポンスを返すこと" do
+      get new_user_session_path
+      expect(response).to be_successful
+      expect(response).to have_http_status "200"
+    end
+
+    it "テスト用ユーザーとしてログインし、正常なレスポンスを返すこと" do
+      sign_in_as user
+      get root_path
+      expect(response).to be_successful
+      expect(response).to have_http_status "200"
+    end
+  end
 end
