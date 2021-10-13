60c1cc3a5d95baf07705e83b5dc91b099c0cae0f
Yuto Kawai <yuto@YutonoMacBook-Air.local>
2021-10-13 13:49:33 +0900

    rubocopの自動修正を実行

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -7,13 +7,13 @@ RSpec.feature "SignUps", type: :feature do
     visit root_path
     click_link "新規登録"
     perform_enqueued_jobs do
-      expect {
-        fill_in "名前",              with: "Example"
-        fill_in "Eメール",     with: "test@example.com"
-        fill_in "パスワード",         with: "test123"
-        fill_in "パスワード(確認)",  with: "test123"
+      expect do
+        fill_in "名前", with: "Example"
+        fill_in "Eメール", with: "test@example.com"
+        fill_in "パスワード", with: "test123"
+        fill_in "パスワード(確認)", with: "test123"
         click_button "新規登録"
-      }.to change(User, :count).by(1)
+      end.to change(User, :count).by(1)
 
       expect(current_path).to eq root_path
     end
