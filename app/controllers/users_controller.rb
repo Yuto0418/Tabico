1a8c492201e2da7a09d53dc27c412f062675185f
Yuto Kawai <yuto@YutonoMacBook-Air.local>
2021-10-21 13:53:16 +0900

    プロフィールの初期フォーマット

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
   def index
   end
+  def profile
+    @user = User.find(current_user.id)
+  end
+  def update
+    @user = User.find(current_user.id)
+    if current_user.update(user_params)
+      flash[:notice] = "ユーザーIDが「#{@user.name}」の情報を更新しました"
+      redirect_to users_profile_path
+    else
+      render :profile
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:image_name, :name)
+  end
 end
