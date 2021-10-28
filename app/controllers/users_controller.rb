e5e45d9c88aef7eb614188a3d234ae561735228b
Yuto Kawai <yuto@YutonoMacBook-Air.local>
2021-10-28 13:07:47 +0900

    profileをshowに書き換え

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,14 @@ class UsersController < ApplicationController
 
   def index
   end
-  def profile
+  def show
     @user = User.find(current_user.id)
   end
   def update
     @user = User.find(current_user.id)
     if current_user.update(user_params)
       flash[:notice] = "ユーザーIDが「#{@user.name}」の情報を更新しました"
-      redirect_to users_profile_path
+      redirect_to action: :show
     else
       flash[:notice] = "情報の更新が失敗しました"
       render :profile
