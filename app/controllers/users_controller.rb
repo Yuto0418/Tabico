3dc7e516e11f495207e6f30d26a6f65f135aad2f
Yuto Kawai <yuto@YutonoMacBook-Air.local>
2021-10-28 13:22:01 +0900

    rubocopを実行

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
-
   def index
   end
+
   def show
     @user = User.find(current_user.id)
   end
+
   def update
     @user = User.find(current_user.id)
     if current_user.update(user_params)
@@ -17,6 +18,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:image_name, :name)
   end
