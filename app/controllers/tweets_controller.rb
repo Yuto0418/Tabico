db53d265e9bcd29c1e1cb546c137958d4a944de5
Yuto Kawai <yuto@YutonoMacBook-Air.local>
2021-11-01 14:54:42 +0900

    tweetにnewとshowを追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,8 +3,11 @@ class TweetsController < ApplicationController
     # @tweets = Tweet.all
   end
   def new
-    # @tweet = Tweet.new
+    @tweet = Tweet.new
   end
 
-
+  private
+    def tweet_params
+      params.require(:tweet).permit(:body, :image, :user_id)
+    end
 end
