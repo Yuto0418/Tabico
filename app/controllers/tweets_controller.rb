0baa0ddac6d7b1659160b35b6a38e3d9a6114087
Yuto Kawai <yuto@YutonoMacBook-Air.local>
2021-11-02 12:08:32 +0900

    tweetコントローラーにcreateとshowを追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,7 +5,19 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
-
+  def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
+    if @tweet.save
+      flash[:notice] = "投稿しました"
+      redirect_to @tweet
+    else
+      render "new"
+    end
+  end
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
   private
     def tweet_params
       params.require(:tweet).permit(:body, :image, :user_id)
