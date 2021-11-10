b7958142306be25457c005e1bc0663848e6bae70
Yuto Kawai <yuto@YutonoMacBook-Air.local>
2021-11-10 13:34:02 +0900

    ゲストユーザーとしてログインできる機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,13 @@ class User < ApplicationRecord
   mount_uploader :image_name, ImageUploader
   has_many :tweets, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.name = "ゲストユーザー"
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
+    end
+  end
 end
