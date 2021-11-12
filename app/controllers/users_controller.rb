808c0f3b0b5c1a343fc5766ba126d9a07a5e0966
Yuto Kawai <yuto@YutonoMacBook-Air.local>
2021-11-12 17:29:18 +0900

    Userモデルにdescription(紹介文)を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:image_name, :name)
+    params.require(:user).permit(:image_name, :name, :description)
   end
 end
